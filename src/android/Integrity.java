f0b76171146564db513136273db416302d303b30
Srihari <nts.srihari@gmail.com>
2024-03-19 13:59:40 +0530

    Update Integrity.java

--- src/android/Integrity.java
+++ src/android/Integrity.java
@@ -18,27 +18,55 @@ import org.json.JSONException;
 
 public class Integrity extends CordovaPlugin {
   @Override public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("check")) {
-      String nonce = data.getString(0);
-      Context context = this.cordova.getActivity().getApplicationContext();
-      if (GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(context) != ConnectionResult.SUCCESS) {
-        callbackContext.error("Play Services not found");
-        return true;
-      }
-      IntegrityManager integrityManager = IntegrityManagerFactory.create(context);
-      Task<IntegrityTokenResponse> integrityTokenResponse = integrityManager.requestIntegrityToken(
-        IntegrityTokenRequest.builder().setNonce(nonce).build());
-      integrityTokenResponse.addOnSuccessListener(integrityTokenResponse1 -> {
-        String integrityToken = integrityTokenResponse1.token();
-        callbackContext.success(integrityToken);
-      });
-      integrityTokenResponse.addOnFailureListener(e -> {
-        callbackContext.error(e.getMessage());
-      });
-      return true;
-    } else {
-      callbackContext.error("Invalid method: " + action);
-      return true;
-    }
+     if (action.equals("requestIntegrityToken")) {
+            requestIntegrityToken(args, callbackContext);
+            return true;
+        }
+        return false;
   }
+  private void requestIntegrityToken(JSONArray args, CallbackContext callbackContext) {
+        try {
+            JSONObject options = args.getJSONObject(0);
+            String nonce = options.getString("nonce");
+            long googleCloudProjectNumber = options.getLong("googleCloudProjectNumber");
+
+            Context context = cordova.getActivity().getApplicationContext();
+            if (GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(context) != ConnectionResult.SUCCESS) {
+                callbackContext.error("Play Services not found");
+                return;
+            }
+            IntegrityManager integrityManager = IntegrityManagerFactory.create(context);
+
+            IntegrityTokenRequest req = IntegrityTokenRequest.builder().setNonce(nonce).build();
+            if (googleCloudProjectNumber != 0) {
+                req = IntegrityTokenRequest.builder().setNonce(nonce).setCloudProjectNumber(googleCloudProjectNumber).build();
+            }
+            Task<IntegrityTokenResponse> integrityTokenResponse = integrityManager.requestIntegrityToken(req);
+            integrityTokenResponse.addOnSuccessListener(
+                new OnSuccessListener<IntegrityTokenResponse>() {
+                    @Override
+                    public void onSuccess(IntegrityTokenResponse integrityTokenResponse1) {
+                        String integrityToken = integrityTokenResponse1.token();
+                        JSONObject ret = new JSONObject();
+                        try {
+                            ret.put("token", integrityToken);
+                            callbackContext.success(ret);
+                        } catch (JSONException e) {
+                            callbackContext.error("Error creating JSON response");
+                        }
+                    }
+                }
+            );
+            integrityTokenResponse.addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        callbackContext.error(e.getMessage());
+                    }
+                }
+            );
+        } catch (JSONException e) {
+            callbackContext.error("Error parsing parameters: " + e.getMessage());
+        }
+    }
 }
